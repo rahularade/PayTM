37b44e3556ec09b6716c1243e5d9477eb217f082
Rahul Arade <araderahul9196@gmail.com>
2025-12-12 18:29:09 +0530

    Remove dist folder

--- backend/dist/config.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.IS_PROD = exports.MONGODB_URL = exports.JWT_SECRET = void 0;
-exports.JWT_SECRET = process.env.JWT_SECRET || "jwtsecret";
-exports.MONGODB_URL = process.env.MONGODB_URL || "mongodb://localhost:27017/paytm";
-exports.IS_PROD = process.env.NODE_ENV === "production";
