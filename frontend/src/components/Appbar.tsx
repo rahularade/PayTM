c7d66e6763e0046365de04016bf09919271b8a1e
Rahul Arade <araderahul9196@gmail.com>
2025-12-15 16:15:19 +0530

    Testing frontend pipeline

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -7,23 +7,26 @@ import { Github } from "./Github";
 
 export function Appbar() {
     const [open, setOpen] = useState(false);
-    const navigate = useNavigate()
-    const { user } = useAuth()
+    const navigate = useNavigate();
+    const { user } = useAuth();
     const ref = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
-        function handleClick(event: MouseEvent){
-            if(ref.current && !ref.current.contains(event.target as Node)){
-                setOpen(false)
+        function handleClick(event: MouseEvent) {
+            if (ref.current && !ref.current.contains(event.target as Node)) {
+                setOpen(false);
             }
         }
 
-        document.addEventListener("mousedown", handleClick)
-        return () => document.removeEventListener("mousedown", handleClick)
-    }, [])
+        document.addEventListener("mousedown", handleClick);
+        return () => document.removeEventListener("mousedown", handleClick);
+    }, []);
 
     return (
-        <div ref={ref} className="flex items-center text-gray-900 justify-between px-20 py-3 border-b-2 border-black bg-white sticky top-0 z-50">
+        <div
+            ref={ref}
+            className="flex items-center text-gray-900 justify-between px-20 py-3 border-b-2 border-black bg-white sticky top-0 z-50"
+        >
             <div
                 className="text-2xl font-black cursor-pointer"
                 onClick={() => navigate("/dashboard")}
@@ -32,28 +35,36 @@ export function Appbar() {
             </div>
             <div className="flex items-center gap-2">
                 <Github />
-                <div className="text-base font-semibold">Hello, {user?.firstName}</div>
-                <div onClick={() => setOpen(prev => !prev)}>
-                    <Avatar letter={user?.firstName[0] || "U"} color="primary" />
+                <div className="text-base font-semibold">
+                    Hello, {user?.firstName}
+                </div>
+                <div onClick={() => setOpen((prev) => !prev)}>
+                    <Avatar
+                        letter={user?.firstName[0] || "U"}
+                        color="primary"
+                    />
                 </div>
             </div>
-            {open && <Accordion user={user!}/>}
+            {open && <Accordion user={user!} />}
         </div>
     );
 }
 
 function Accordion({ user }: { user: UserType }) {
-    const {setUser} = useAuth()
+    const { setUser } = useAuth();
     return (
         <div className="absolute right-20 top-18 w-40 bg-white border border-slate-200 rounded shadow-md font-semibold">
             <ul className="py-2">
                 <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">
                     {user?.firstName} {user?.lastName}
                 </li>
-                <li onClick={() => {
-                    localStorage.removeItem("token")
-                    setUser(null)
-                }} className="px-4 py-2 hover:bg-gray-100 cursor-pointer text-red-500">
+                <li
+                    onClick={() => {
+                        localStorage.removeItem("token");
+                        setUser(null);
+                    }}
+                    className="px-4 py-2 hover:bg-gray-100 cursor-pointer text-red-500"
+                >
                     Logout
                 </li>
             </ul>
